d927e2b1a19bbf528d2538d27a9e693db23d6e37
RafalKielbasa <kielbasarafal92@gmail.com>
2024-02-27 19:37:22 +0100

    project-setup-vite

--- README.md
+++
@@ -1,1 +0,0 @@
-# zrozumie--react
---
+++ zrozumiec-react-vite-js/src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import './index.css'
+
+const element = <div>Tekst</div>
+
+ReactDOM.createRoot(document.getElementById('root')).render(element)
